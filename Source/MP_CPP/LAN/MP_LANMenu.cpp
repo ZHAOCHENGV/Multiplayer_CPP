623d1a29271a8915a28e8e3b698b9a9ecd87f16e
V <zcw1419@gmail.com>
2025-09-30 00:02:41 +0800

    第一次提交
    第一次提交

---
+++ Source/MP_CPP/LAN/MP_LANMenu.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MP_LANMenu.h"
+
+#include "Components/Button.h"
+#include "Components/EditableTextBox.h"
+#include "Kismet/GameplayStatics.h"
+
+void UMP_LANMenu::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	FInputModeUIOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(true);
+
+	Button_Host->OnClicked.AddDynamic(this, &ThisClass::HostButtonClicked);
+	Button_Join->OnClicked.AddDynamic(this, &ThisClass::JoinButtonClicked);
+}
+
+void UMP_LANMenu::HostButtonClicked()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	UGameplayStatics::OpenLevelBySoftObjectPtr(this, HostingLevel, true, TEXT("监听模式"));
+}
+
+void UMP_LANMenu::JoinButtonClicked()
+{
+	FInputModeGameOnly InputMode;
+	GetOwningPlayer()->SetInputMode(InputMode);
+	GetOwningPlayer()->SetShowMouseCursor(false);
+
+	const FString Address = TextBox_IpAddress->GetText().ToString();
+	UGameplayStatics::OpenLevel(this, *Address);
+}
